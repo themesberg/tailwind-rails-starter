7d53f89057dc7942b0dd54b26270586cc6c87c6f
Zoltán Szőgyényi <szogyenyi.zoltan.lorand@gmail.com>
2025-03-18 14:52:44 +0200

    initial commit

---
+++ .bash_profile
@@ -0,0 +1,3 @@
+export PATH="/usr/local/heroku/bin:$PATH" 
+export PATH="$HOME/.rvm/bin:$PATH"
+
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,43 @@
+class PagesController < ApplicationController
+    def home
+    end
+
+    def accordion
+    end
+
+    def carousel
+    end
+
+    def modal
+    end
+
+    def collapse
+    end
+
+    def dial
+    end
+
+    def dismiss
+    end
+
+    def drawer
+    end
+
+    def dropdown
+    end
+
+    def popover
+    end
+
+    def tabs
+    end
+
+    def tooltip
+    end
+
+    def input_counter
+    end
+
+    def datepicker
+    end
+  end
